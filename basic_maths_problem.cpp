c363b128d2de8853057c166ccdb1d687c886f8e1
a-r-nida <r.nidaanjum71@gmail.com>
2025-03-04 15:20:21 +0530

    adding new problem

--- basic_maths_problem.cpp
+++ basic_maths_problem.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
+// problem1: Function to count the number of digits in n that evenly divide n
+int evenlyDivides(int n) {
+    int rem;
+    int remaining=n,ans=0;
+    while(true){
+        if(remaining!=0)
+        return ans;
+        rem=remaining%10;
+        if(rem!=0 && n%rem==0){
+        ans++;
+        }
+        remaining/=10;
+      
+        
+    }
+    return ans;
+}
 int main(){
     cout<<"here are some basic maths problems"<<endl;
-    //problem1: count digit
-}
+    int p1=evenlyDivides(23456);
+    }
